74dfdb275a3ad33a43a85ba37298bc6c9c16da58
lucethegoose <81484215+lucethegoose@users.noreply.github.com>
2021-04-02 16:36:15 +0100

    CV as a dictionary of lists

---
+++ src/CV.py
@@ -0,0 +1,11 @@
+# Controlled Vocabulary - faceted CV
+
+CV = {}
+
+CV["Region"]=["UK","Europe","Asia","Africa","North America","South America","Middle East","Oceania"]
+CV["Subject"]=["Event","Person","Object"]
+CV["Culture"]=["Tv", "Music", "Film", "Book", "Theatre", "Art"]
+CV["Sport"]=["Football", "Rugby", "F1", "Cricket", "Cycling", "Tennis"]
+CV["LifeStyle"]=["Fashion", "Food", "Health"]
+
+print(CV)
