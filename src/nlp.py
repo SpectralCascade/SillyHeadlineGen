31222b3c736c731a3e12f67e3429215ddc289ac6
Tim Lane <timlanesoftware@gmail.com>
2021-04-14 17:50:50 +0100

    Also process names

--- src/nlp.py
+++ src/nlp.py
@@ -19,34 +19,32 @@ WH_WORDS = {"WP", "WP$", "WRB"}
 #from mlxtend.preprocessing import TransactionEncoder
 
 def determine_subject_type(subject):
-    #splitted = subject.split(" ");
+    splitted = subject.split(" ");
 
     # First check if it's a name or not
-    #name_database = NameDatasetV1()
-    #if (name_database.search_first_name(splitted[0]) or name_database.search_last_name(splitted[0]) or (len(splitted) > 1 and (name_database.search_first_name(splitted[-1])  or name_database.search_last_name(splitted[-1])))):
-    #    # Must be a person
-    #    return "person"
-    #else:
-    #subject = subject.lower()
-    
-    with open('data/world-cities.csv', encoding='utf-8') as csv_file:
-        reader = csv.reader(csv_file, delimiter=',')
-        for row in reader:
-            if (subject == row[0]):
-                # Is a city!
-                return "city"
-    
-    # Check the big database - needs extraction first
-    with zipfile.ZipFile('data/all_countries_dict.zip', 'r') as zipObj:
-        zipObj.extractall()
-    
-    with open('all_countries_dict.json', encoding='utf-8') as json_file:
-        loaded = json.load(json_file)
-        if (subject in loaded):
-            # Is a place!
-            return "place"
-                
-    return "unknown"
+    name_database = NameDatasetV1()
+    if (name_database.search_first_name(splitted[0]) or name_database.search_last_name(splitted[0]) or (len(splitted) > 1 and (name_database.search_first_name(splitted[-1])  or name_database.search_last_name(splitted[-1])))):
+        # Must be a person
+        return "person"
+    else:
+        with open('data/world-cities.csv', encoding='utf-8') as csv_file:
+            reader = csv.reader(csv_file, delimiter=',')
+            for row in reader:
+                if (subject == row[0]):
+                    # Is a city!
+                    return "city"
+        
+        # Check the big database - needs extraction first
+        with zipfile.ZipFile('data/all_countries_dict.zip', 'r') as zipObj:
+            zipObj.extractall()
+        
+        with open('all_countries_dict.json', encoding='utf-8') as json_file:
+            loaded = json.load(json_file)
+            if (subject in loaded):
+                # Is a place!
+                return "place"
+                    
+        return "unknown"
 
 def nlp_extract(headline):
     # First use spacy to extract SVO
@@ -66,9 +64,16 @@ def nlp_extract(headline):
         if token.dep_ in SUBJECT_DEPS or token.head.dep_ in SUBJECT_DEPS:
             subjects.append(token.text)
 
+
+    '''for verb in verbs:
+        print("Found verb " + verb)
+        print(determine_subject_type(verb))'''
+    for object in objects:
+        print("Found object " + object)
+        print(determine_subject_type(object))
     for subject in subjects:
         print("Found subject " + subject)
         #for word in subject:
         print(determine_subject_type(subject))
 
-nlp_extract("Denmark suspends use of Oxford-AZ Covid jab")
+nlp_extract("Sadiq Khans guide to a legal weed London")
