4e5accbf5a3ad62ac1c461bd35851b4d858176dc
lucethegoose <81484215+lucethegoose@users.noreply.github.com>
2021-04-19 17:47:29 +0100

    no more random headline

--- src/userConsole.py
+++ src/userConsole.py
@@ -1,13 +1,11 @@
 userInput = int(input("Hello, this is the parody headline checker!S \n To begin choose between:" \
-                  "\n 1.Input a URL \n 2. Choosing a random headline from a category" \
-                  "\n 3. Input a headline" \
-                  "\n Choose 1, 2 or 3 \n ")) 
+                  "\n 1.Input a URL" \
+                  "\n 2. Input a headline" \
+                  "\n Choose 1 or 2 \n ")) 
 
 if userInput == 1:
     print("You've picked to input a URL")
 elif userInput == 2:
-    print("You've picked to choose a category to generate a random headline")
-elif userInput == 3:
     print("You've picked to input a headline")
 else:
     print("Choose either 1 2 or 3")
