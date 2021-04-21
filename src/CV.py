31e9f1f8986e255dbde4341be68b9c4c4727935f
lucethegoose <81484215+lucethegoose@users.noreply.github.com>
2021-04-21 16:27:52 +0100

    smush sport

--- src/CV.py
+++ src/CV.py
@@ -3,10 +3,9 @@
 CV = {}
 
 # CV["Region"]=["UK","Europe","Asia","Africa","North America","South America","Middle East","Oceania"]
-# updated CV, UK in europe, and culture/lifestyle merged
-CV["Region"]=["UK","Europe","Asia","Africa","North America","South America","Middle East","Australia"]
+# updated CV, UK in europe, and culture/lifestyle merged, sport is put into culter as a broad term
+CV["Region"]=["Europe","Asia","Africa","North America","South America","Middle East","Australia"]
 CV["Subject"]=["Event","Person","Object"]
-CV["Culture"]=["Music", "Film", "Book", "Theatre", "Fashion", "Food", "Health"]
-CV["Sport"]=["Football", "Rugby", "F1", "Cricket", "Cycling", "Tennis"]
+CV["Culture"]=["Music", "Film", "Book", "Food", "Health", "Sport"]
 
 print(CV)
