ac4208616d17ad745425c2277a41090d89d62217
Tim Lane <timlanesoftware@gmail.com>
2021-04-21 18:12:53 +0100

    Categorise headlines

--- src/soupScraping.py
+++ src/soupScraping.py
@@ -1,19 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import CV from CV
+import string
 
 def categoriseArticle(headline, content):
+    categories = dict()
     cv_map = dict()
     
     region = "Region"
     cv_map[region] = dict()
     with open("data/Countries-Continents.csv") as csvfile:
         reader = csv.reader(csvfile)
+        next(reader, None)
         for row in reader:
-            
-    for term in CV[region]:
-        cv_map[region][term] = dict()
+            if (row[0] not in cv_map[region]):
+                cv_map[region][row[0]] = dict()
+            cv_map[region][row[0]][row[1]] = 1
+    
+    # Extract words from article headline and content
+    # TODO: extract full subjects/objects/nouns so full country names work
+    words = []
+    for word in headline.split():
+        word = word.translate(str.maketrans('', '', string.punctuation))
+        words.append(word)
+    for word in content.split():
+        word = word.translate(str.maketrans('', '', string.punctuation))
+        words.append(word)
+    
+    # Top level terms (i.e. Region, Culture and Subject)
+    for word in words:
+        for key in cv_map:
+            terms = cv_map[key]
+            if (word == "Europe"):
+                print("Europe is here! Terms = " + str(terms))
+                wait = input()
+            if (word in terms):
+                if (word not in categories):
+                    categories[word] = 1
+                categories[word] += 1
+                break
+            # Inner terms, e.g. Europe, Africa, Asia
+            # with subterms e.g. Spain, Portugal, France
+            for inner_terms in terms:
+                subterms = terms[inner_terms]
+                if (word in subterms):
+                    if (word not in categories):
+                        categories[term] = 1
+                    categories[word] += 1
+                break
+    
+    return categories
 
 def chaserScrape():
     for i in range(10):
@@ -28,13 +64,14 @@ def chaserScrape():
                 headlineString = headline.text
                 writer.writerow([headlineString])
 
-
+# TODO: extract article contents
 def dailymashScrape():
     for i in range(10):
         url = (f'https://www.thedailymash.co.uk/news/page/{i}')
         page = requests.get(url)
         soup1 = BeautifulSoup(page.content, 'html.parser')
 
+        print("\nPage " + str(i) + "\n")
         with open('dailymashHeadlines.csv', 'a', newline='') as file:
             writer = csv.writer(file)
             headlines = soup1.find_all('div', {'class': 'holder'})
@@ -43,6 +80,9 @@ def dailymashScrape():
                 for headline2 in headlineStripped:
                     headlineString = headline2.text
                     writer.writerow([headlineString])
+                    # test categorisation
+                    print("Headline: " + headlineString + " | Categories: " + str(categoriseArticle(headlineString, "")))
+                    
 
 
 def beavertonScrape():
@@ -60,3 +100,5 @@ def beavertonScrape():
                 for headline2 in headlineStripped:
                     headlineString = headline2.text
                     writer.writerow([headlineString])
+
+dailymashScrape()
