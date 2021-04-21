062dbde45d713c26d51019040208fd657fdf9688
OnlyKalvin <kalvin.willis1@gmail.com>
2021-04-21 15:42:16 +0100

    Basic XML creation

--- src/mrOutput.py
+++ src/mrOutput.py
@@ -1,6 +1,9 @@
 import json
+import xml.etree.ElementTree as ET 
+#Reference: https://docs.python.org/3/library/xml.etree.elementtree.html
+#Accessed 21/04/2021 at 14:45
 
-#takes input from the main program and posts it in a JSON file as a dictionary
+
 
 class mrOutput:
 	#inputDict = {} <---- will be populated from elsewhere in the program
@@ -9,12 +12,23 @@ class mrOutput:
 		"Tasty": "78%",
 		"Fruity":"0%"
 	}
-
+	#takes input from the main program and posts it in a JSON file as a dictionary
 	def createJSON(inputDict):
 		#open a new file and dump the dictionary to it
 		with open('outputData.json', 'w') as outputFile:
 			json.dump(inputDict, outputFile, indent=4)
 
+	def createXML(inputDict):
+		title = ET.Element(inputDict.get("Title"))
+		identifier1 = ET.SubElement(title, "Tasty : " + inputDict.get("Tasty"))
+		identifier2 = ET.SubElement(title, "Fruity : " + inputDict.get("Fruity"))
+		finalTree = ET.ElementTree(title)
+		with open('outputData.xml', 'w') as outputFile:
+			finalTree.write(outputFile, encoding='unicode')
+
+
+
 	if  __name__ == "__main__":
-		#run the createJSON method
+		#run the createJSON and createXML method
 		createJSON(exampleDict)
+		createXML(exampleDict)
