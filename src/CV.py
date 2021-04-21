93d57242188e1ff94138410b1d5cf64b142f98a2
lucethegoose <81484215+lucethegoose@users.noreply.github.com>
2021-04-21 16:23:46 +0100

    Update CV.py

--- src/CV.py
+++ src/CV.py
@@ -4,7 +4,7 @@ CV = {}
 
 # CV["Region"]=["UK","Europe","Asia","Africa","North America","South America","Middle East","Oceania"]
 # updated CV, UK in europe, and culture/lifestyle merged
-CV["Region"]=["UK","Europe","Asia","Africa","North America","South America","Middle East","Oceania"]
+CV["Region"]=["UK","Europe","Asia","Africa","North America","South America","Middle East","Australia"]
 CV["Subject"]=["Event","Person","Object"]
 CV["Culture"]=["Music", "Film", "Book", "Theatre", "Fashion", "Food", "Health"]
 CV["Sport"]=["Football", "Rugby", "F1", "Cricket", "Cycling", "Tennis"]
