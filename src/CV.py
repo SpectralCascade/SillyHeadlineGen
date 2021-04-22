b3c10fb05e31f282b800321d1527ff7e54a2c6d4
lucethegoose <81484215+lucethegoose@users.noreply.github.com>
2021-04-22 16:45:35 +0100

    update CV into dict

--- src/CV.py
+++ src/CV.py
@@ -1,9 +1,12 @@
 # Controlled Vocabulary - faceted CV
 
-CV = {}
-# updated CV, UK in europe, and culture/lifestyle merged, sport is put into culter as a broad term
-CV["Region"]=["Europe","Asia","Africa","North America","South America","Oceania"]
-CV["Subject"]=["Event","Person","Object"]
-CV["Culture"]=["Entertainment", "Food", "Health", "Sport","Politics"]
+#CV = {}
+#CV["Region"]=["Europe","Asia","Africa","North America","South America","Oceania"]
+#CV["Subject"]=["Event","Person","Object"]
+#CV["Culture"]=["Entertainment", "Food", "Health", "Sport","Politics"]
+
+CV = { "Region" : [ "Europe","Asia","Africa","North America","South America","Oceania"],
+      "Subject" : ["Event","Person","Object"] ,
+       "Culture" : ["Entertainment", "Food", "Health", "Sport","Politics"]}
 
 print(CV)
