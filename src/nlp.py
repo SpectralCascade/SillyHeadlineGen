287cd1e33d0e11c2db98a01ee5f9666e3d80384a
Tim Lane <timlanesoftware@gmail.com>
2021-04-26 21:00:38 +0100

    Fixed CSV file, added sentiment and adjective appearance attribute

--- src/nlp.py
+++ src/nlp.py
@@ -14,42 +14,43 @@ def GetHeadlineNLP():
 
 class HeadlineNLP:
     instance = None
-
+    
     # Initialise the singleton instance
     def __init__(self):
         if (HeadlineNLP.instance == None):
             # Kinda-singleton
             HeadlineNLP.instance = self
             print("Initialising HeadlineNLP...")
-
+            
             # Load NLP
             self.nlp = spacy.load("en_core_web_sm")
             # This makes sure entities such as person names are joined together properly
             self.nlp.add_pipe("merge_entities")
-
+            
             # Dictionary of entity types to proper nouns
             # Note, PERSON is not included because the names database is a little more complex
             self.database = {"GPE" : set()}
-
+            
             # Initialise the name database
             self.name_db = NameDatasetV1()
-
+            
             # Setup entity database
             with open('data/world-cities.csv', encoding='utf-8') as csv_file:
                 reader = csv.reader(csv_file, delimiter=',')
                 for row in reader:
                     self.database["GPE"].add(row[0])
-
-            if (not os.path.isfile("./all_countries_dict.json")):
+            
+            # Removed "all countries" for now as it takes a long time to load and spaCy should still detect common country names
+            '''if (not os.path.isfile("./all_countries_dict.json")):
                 # Country names database is so big it has to be compressed for git
                 with zipfile.ZipFile('data/all_countries_dict.zip', 'r') as zipObj:
                     zipObj.extractall()
-
+            
             with open('all_countries_dict.json', encoding='utf-8') as json_file:
                 loaded = json.load(json_file)
                 for key in loaded:
-                    self.database["GPE"].add(key)
-
+                    self.database["GPE"].add(key)'''
+            
             print ("HeadlineNLP initialised!")
 
     # For entities which aren't identified by spaCy, some additional processing is needed.
@@ -72,7 +73,7 @@ class HeadlineNLP:
             print(ent.text + " => " + ent.label_ + " (" + spacy.explain(ent.label_) + ")")
         print("\nTokens:")
         spans = []
-
+        
         for token in doc:
             pos = spacy.explain(token.pos_)
             dep = spacy.explain(token.dep_)
@@ -81,7 +82,7 @@ class HeadlineNLP:
             if (dep == None):
                 dep = "unknown"
             print(token.text + " | " + token.pos_ + " (" + pos + ") | " + token.dep_ + " (" + dep + ")" + " | Head: " + token.head.text + " | Children: \"" + str([t.text for t in token.children]) + "\"")
-
+    
     # Extracts entities and verbs from a headline into a dictionary
     def nlp_extract(self, headline):
         data = dict()
@@ -115,13 +116,13 @@ class HeadlineNLP:
         for key in entities:
             if (entities[key] == "UNKNOWN"):
                 entities[key] = self.determine_subject_type(key)
-
+        
         return data
 
 if (__name__ == "__main__"):
-    to_analyse = "John Smith: \'I was sacked in Exeter :('"
-
+    to_analyse = "UK denies that PM said 'Let the bodies pile high in their thousands'"
+    
     if (len(sys.argv) > 1):
         to_analyse = sys.argv[1]
-
+    
     print(GetHeadlineNLP().nlp_extract(to_analyse))
